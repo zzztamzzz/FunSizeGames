ab6ff337e545770fb999d20edb1eed47e694fd22
Tamzid Choudhury <choudhury.tamzid@gmail.com>
2022-10-20 14:33:17 -0400

    Update game1.cpp

--- Cpp/Game1/game1.cpp
+++ Cpp/Game1/game1.cpp
@@ -1,63 +1,38 @@
-// 1D game, "Life Sprint".
-
+/*
+Rock, Paper and Scissor game
+There will be a 1 in 3 change of guessing correctly
+User should be able to choose a response
+*/
 #include<iostream>
-#include<conio.h>
-
+#include<cmath>
 using namespace std;
-bool gameOver;
-const int width = 20;
-const int height = 16;
 
-int a, b, foodX, foodY, specialfood, scoreCount;
-int tailX[100], tailY[100];
-int nTail;
-enum eDirection {STOP = 0, LEFT,RIGHT, UP, DOWN}; // Controls
-eDirection dir;
+// Function Prototypes
+int diceGame();
 
-void setup(){
-    gameOver = false;
-    dir = STOP;
-    a = width/2;
-    b = height/2;
-    // Spawn fruit for snake to eat
-    foodX = rand() % width;
-    foodY = rand() % height; 
-    scoreCount = 0;
-    for(int i = 0; i < width + 2; i++){
-        cout << "#" << endl; 
-    }
-    for(int i = 0; i < width; i++){
-        for(int j = 0; j < height; j++){
-            if(j == 0){
-                cout << "#"; // walls
-            }
-            if(j == a && i == b){
-                cout << "*"; // snake tail
-            }
-            else if(j == foodY && i == foodX){
-                cout << "%";
-            }
-            bool print = false;
-            for (int k = 0; k < nTail; k++){
-                if(tailX[k] == j && tailY[k] == i){
-                    cout << "*";
-                    print = true;
-                }
-            }
-            if(!print){
-                cout << " ";
-            }
-            if(j == width -1){
-                cout << "#" << endl;
-                cout << "Score: " << scoreCount << endl;
-            }
-        }
-    }
-}
-void Input(){
-    
-}
 int main(){
-    cout << "Welcome to Life Sprint!" << endl;
+    cout << "Are you ready to play?(Choose the option number)\n1. Play.\n2 Exit." << endl;
+    int response = 0;
+    cin >> response;
+    do
+    {
+        /* code */
+        cout << "Invalid response. Please enter 1 or 2!" << endl;
+    } while (response > 2);
+    if(response == 1){
+        cout << "What kind of game would you like to play?\n1. Dice Game\n2. Rocks, papers & scissors.\n3. Flip a coin." << endl;
+    }
+}
 
+/*
+Dice game: You enter the number that could show up on the dice.
+There is a 1 in 6 chance to get the correct response.
+Play for 6 rounds, ie, roll rice 6 times and guess 6 times.
+Keep track of score.
+*/
+int diceGame(){
+    int diceFace = 0, score = 0;
+    for(int unsigned i = 0; i <= 6; i++){
+        srand(i);
+    }
 }
