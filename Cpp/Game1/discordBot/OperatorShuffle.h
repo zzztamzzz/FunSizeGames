4a64e03387c9892bd6d1b3c793fb5166d3997e7c
Tamzid Choudhury <choudhury.tamzid@gmail.com>
2023-01-24 13:34:30 -0500

    started

---
+++ Cpp/Game1/discordBot/OperatorShuffle.h
@@ -0,0 +1,18 @@
+/*
+Tamzid Choudhury
+Header file consisting R6S operator structure
+*/
+
+#ifndef OPERATORSHUFFLE_H
+#define OPERATORSHUFFLE_H
+
+#include <string>
+using namespace std;
+
+struct OpFOR
+{
+    // Operator name
+    string name = "";
+};
+
+#endif
