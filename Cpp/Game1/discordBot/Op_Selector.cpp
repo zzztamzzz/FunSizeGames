c27ac84d8d08e0e8742f32469db548ed7c0c416d
Tamzid Choudhury <choudhury.tamzid@gmail.com>
2023-01-25 15:22:06 -0500

    Update Op_Selector.cpp

--- Cpp/Game1/discordBot/Op_Selector.cpp
+++ Cpp/Game1/discordBot/Op_Selector.cpp
@@ -1,6 +1,7 @@
 #include "Op_Selector.h"
 #include <iostream>
 #include <vector>
+#include <random>
 
 using namespace std;
 
@@ -31,12 +32,28 @@ vector <OpFORS> attackerCollection{
     {"IQ", "Primary 1"},
     {"IQ", "Primary 2"},
     {"Blitz", ""},
+    {"Buck", "AR"},
+    {"Buck", "DMR"},
+    {"BlackBeard", "DMR"},
+    {"BlackBeard", "AR"},
+    {"Capitao", "LMG"},
+    {"Capitao", "AR"},
+    {"Hibana", "AR"},
+    {"Hibana", "Shotgun"},
+    {"Jackal", "AR"},
+    {"Jackal", "Shotgun"}
 };
 // Container 2: All available defenders to choose from [33]
 vector <OpFORS> defenderCollection;
 // Method 1: Pick a random attacker
 void attackerChoice(){
-
+    // Select a random element from "attackerCollection"
+    srand(0);
+    int attColSize = attackerCollection.size(); // Position of element in the colleciton
+    // Obtain a random index number
+    int randomlySelectedOPindex = rand() % attColSize;
+    // Print the element (the op) at that position
+    return attackerCollection[randomlySelectedOP];
 };
 // Method 2: Pick a random defender
 void defenderChoice(){
