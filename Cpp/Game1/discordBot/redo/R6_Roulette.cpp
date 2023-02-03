67935e8ee5f6f781ce05336fbb63556c74000460
Tamzid Choudhury <choudhury.tamzid@gmail.com>
2023-02-02 22:15:51 -0500

    hardcoded ops

--- Cpp/Game1/discordBot/redo/R6_Roulette.cpp
+++ Cpp/Game1/discordBot/redo/R6_Roulette.cpp
@@ -8,11 +8,35 @@
 using namespace std;
 // Container 1: Choice of attackers
 vector <string> attackerChars{
-    ""
+    "Sledge","Thatcher",
+    "Ash","Thermite",
+    "Twitch","Montagne",
+    "Fuze","Glaz",
+    "IQ","Blitz",
+    "Buck", "Blackbeard", "Capitao",
+    "Hibana", "Jackal", "Ying",
+    "Dokkaebi", "Finka", "Lion",
+    "Maverick", "Nomad", "Gridlock",
+    "Nokk", "Amaru", "Kali",
+    "Iana", "Kali", "Ace",
+    "Zero", "Flores", "Osa",
+    "Sens", "Grim"
 };
 // Container 2: Choice of defenders
 vector <string> defenderChars{
-    ""
+    "Smoke", "Mute",
+    "Castle", "Pulse",
+    "Doc", "Rook",
+    "Jager", "Bandit",
+    "Kapkan", "Tchanka",
+    "Frost", "Valkyrie", "Caveria",
+    "Echo", "Mira", "Lesion", "Ela"
+    "Vigil", "Maestro", "Alibi",
+    "Clash", "Kaid", "Mozzie",
+    "Warden", "Goyo", "Wamai",
+    "Oryx", "Melusi", "Aruni",
+    "Thunderbird", "Thorn",
+    "Azami", "Solis"
 };
 
 // Method 1, return a randomly selected attacker
