d7cfbd74ae59d452df6a9ab827391d5fa0597673
Tamzid Choudhury <choudhury.tamzid@gmail.com>
2024-01-08 14:59:45 -0500

    auto

--- test_programs/test_cpp/prog1.cpp
+++ test_programs/test_cpp/prog1.cpp
@@ -25,19 +25,75 @@ int addition(){
 }
 
 int subtraction(){
-  
+  int a, b;
+  std::cout << "Give me two numbers:" << std::endl;
+  std::cin >> a >> b;
+  int subtraction = a - b;
+  std::cout << "The difference between the numbers: " << subtraction << std::endl;
+  return 0;
+}
+
+int multiply(){
+  int a, b;
+  std::cout << "Give me two numbers to multiply:" << std::endl;
+  std::cin >> a >> b;
+  int multiplied = a * b;
+  std::cout << "The product of the numbers: " << multiplied << std::endl;
+  return 0;
+}
+
+int divide(){
+  int a, b;
+  std::cout << "Give me two numbers to divide:" << std::endl;
+  std::cin >> a >> b;
+  int divided = a / b;
+  std::cout << "The division of the numbers: " << divided << std::endl;
+  return 0;
 }
 
 int main(){
-  std::cout << "Hello, I am basic calculator. Select one of the options:" << std::endl;
-  std::cout << "1. Addition\n2. Subtraction\n3. Multiplication\n4.Division\n5.Pi" << std::endl;
-  int choice;
-  std::cin >> choice;
-  if (choice == 1){
-    addition();
-  }
-  else{
-    std::cout << "Functionality yet to be implemented" << std::endl;
-  }
-  return 0;
+  // std::cout << "Hello, I am basic calculator. Please limit to two numbers. Select one of the options:" << std::endl;
+  // std::cout << "1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Pi" << std::endl;
+  // int choice;
+  // std::cin >> choice;
+  // if (choice == 1){
+  //   addition();
+  // }
+  // if (choice == 2){
+  //   subtraction();
+  // }
+  // if (choice == 3){
+  //   multiply();
+  // }
+  // if (choice == 4){
+  //   divide();
+  // }
+  // else{
+  //   std::cout << "Functionality yet to be implemented" << std::endl;
+  // }
+  // return 0;
+  std::cout << "Hello, I am basic calculator. Please limit to two numbers. Select one of the options:" << std::endl;
+  std::cout << "1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Pi" << std::endl;
+  int choice = 0;
+
+  do
+  {
+    // int choice = 0;
+    std::cin >> choice;
+    if (choice == 1){
+      addition();
+    }
+    if (choice == 2){
+      subtraction();
+    }
+    if (choice == 3){
+      multiply();
+    }
+    if (choice == 4){
+      divide();
+    }
+    else {
+      std::cout << "Functionality not available" << std::endl;
+    }
+  } while (choice != 0); 
 }
